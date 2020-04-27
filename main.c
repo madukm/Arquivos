70a007242f974e37758581c50d4650cad6461b91
Marcus Medeiros <39343824+MarcusMedeiros99@users.noreply.github.com>
2020-04-27 19:33:31 -0300

    Remoção de código inútil na main

--- main.c
+++ main.c
@@ -4,15 +4,8 @@
 
 int main(int argc, char const *argv[])
 {
-    int tam_cidade;
-    char cidade[105];
-
     FILE* fp = fopen("./teste.bin", "w+b");
 
-    scanf("%[^\n]105s", cidade);
-    printf("%s\n", cidade);
-
-    escreve_cidade(fp,cidade, strlen(cidade));
 
     fclose(fp);
 
